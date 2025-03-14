93675f034d0eeeaabcab53e804bca872cc66f3cc
MihailRubtsov <whitefallenangel02@gmail.com>
2025-03-14 22:07:15 +0300

    создал базовые файлы

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+class Student{
+public:
+    string name;
+    int num_group;
+    float mean_score;
+};
+
+int main(){
+
+    return 0;
+}
